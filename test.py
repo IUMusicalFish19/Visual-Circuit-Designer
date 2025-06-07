baa0d55f73a2f4cf7589949a0dba2af2e0b0ccc2
doshq <matvey4ikk@gmail.com>
2025-06-07 22:46:34 +0300

    Init commit

--- test.py
+++ test.py
@@ -1,1 +1,17 @@
-print("Hello world")
+import cocotb
+from cocotb.triggers import Timer
+from itertools import product
+
+@cocotb.test()
+async def full_circuit_test(dut):
+    inputs = ['A', 'B', 'C']
+    outputs = ['OUT1', 'OUT2']
+
+    for combo in product([0, 1], repeat=len(inputs)):
+        for name, val in zip(inputs, combo):
+            setattr(dut, name, val)
+
+        await Timer(1, units='ns')
+
+        out_vals = [int(getattr(dut, out)) for out in outputs]
+        print(f"Inputs: {combo} => Outputs: {out_vals}")
